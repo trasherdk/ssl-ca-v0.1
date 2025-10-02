74eece4e2c406cd5e16ad06c5b6c82839cff890a
trasherdk <trasherdk@gmail.com>
2025-10-02 07:53:14 +0700

    Refactor script for improved quoting and readability

--- gen-root-ca-crl.sh
+++ gen-root-ca-crl.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 
-BASE=$(realpath $(dirname $0))
-cd ${BASE}
+BASE=$(realpath "$(dirname "$0")")
+cd "${BASE}" || exit 1
 
 CA="${BASE}/CA"
 CONFIG="${BASE}/config/root-ca.conf"
 CRL="${BASE}/CRL/root-ca.crl.pem"
 
-if [ ! -d $(dirname "${CRL}") ]; then
-  mkdir $(dirname "${CRL}") || exit 1
+if [ ! -d "$(dirname "${CRL}")" ]; then
+  mkdir "$(dirname "${CRL}")" || exit 1
 fi
 
-openssl ca -config ${CONFIG} \
-  -gencrl -out ${CRL}
+openssl ca -config "${CONFIG}" \
+  -gencrl -out "${CRL}"
 
-if [ -f ${CRL} ]; then
-  openssl crl -in ${CRL} -noout -text
+if [ -f "${CRL}" ]; then
+  openssl crl -in "${CRL}" -noout -text
 fi
